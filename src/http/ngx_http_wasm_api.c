d93f4ed20020fb657b39336adda5dfefe4513188
spacewander <spacewanderlzx@gmail.com>
2021-09-14 12:29:33 +0800

    refactor: use macro to simplify definition

--- src/http/ngx_http_wasm_api.c
+++ src/http/ngx_http_wasm_api.c
@@ -1,3 +1,4 @@
+#include "vm/vm.h"
 #include "ngx_http_wasm_api.h"
 
 
@@ -27,4 +28,55 @@ ngx_http_wasm_host_api_func(const ngx_wasm_host_api_t *api)
     wasm_valtype_delete(result[0]);
 
     return f;
+}
+
+
+int32_t
+proxy_set_effective_context(int32_t id)
+{
+    /* dummy function to make plugin load */
+    return PROXY_RESULT_OK;
+}
+
+
+int32_t
+proxy_log(int32_t log_level, int32_t addr, int32_t size)
+{
+    const u_char *p;
+    ngx_uint_t    host_log_level = NGX_LOG_ERR;
+
+    p = ngx_wasm_vm.get_memory(ngx_cycle->log, addr, size);
+    if (p == NULL) {
+      return PROXY_RESULT_INVALID_MEMORY_ACCESS;
+    }
+
+    switch (log_level) {
+      case PROXY_LOG_CRITICAL:
+        host_log_level = NGX_LOG_EMERG;
+        break;
+
+      case PROXY_LOG_ERROR:
+        host_log_level = NGX_LOG_ERR;
+        break;
+
+      case PROXY_LOG_WARN:
+        host_log_level = NGX_LOG_WARN;
+        break;
+
+      case PROXY_LOG_INFO:
+        host_log_level = NGX_LOG_INFO;
+        break;
+
+      case PROXY_LOG_DEBUG:
+        host_log_level = NGX_LOG_DEBUG;
+        break;
+
+      case PROXY_LOG_TRACE:
+        host_log_level = NGX_LOG_DEBUG;
+        break;
+    }
+
+    ngx_log_error(host_log_level, ngx_cycle->log, 0, "%*s", size, p);
+
+    return PROXY_RESULT_OK;
 }
--- src/http/ngx_http_wasm_api.h
+++ src/http/ngx_http_wasm_api.h
@@ -5,76 +5,47 @@
 #include <wasm.h>
 #include <wasmtime.h>
 #include <ngx_core.h>
-#include "vm/vm.h"
 #include "ngx_http_wasm_types.h"
 
 
-static wasm_trap_t* proxy_set_effective_context(
-    void *env,
-    wasmtime_caller_t *caller,
-    const wasmtime_val_t *args,
-    size_t nargs,
-    wasmtime_val_t *results,
-    size_t nresults
-) {
-    /* dummy function to make plugin load */
-    return NULL;
-}
-
-
-static wasm_trap_t* proxy_log(
-    void *env,
-    wasmtime_caller_t *caller,
-    const wasmtime_val_t *args,
-    size_t nargs,
-    wasmtime_val_t *results,
-    size_t nresults
-) {
-    int32_t log_level = args[0].of.i32;
-    int32_t addr = args[1].of.i32;
-    int32_t size = args[2].of.i32;
-    const u_char *p;
-    ngx_uint_t host_log_level = NGX_LOG_ERR;
-
-    p = ngx_wasm_vm.get_memory(ngx_cycle->log, addr, size);
-    if (p == NULL) {
-      results[0].kind = WASMTIME_I32;
-      results[0].of.i32 = PROXY_RESULT_INVALID_MEMORY_ACCESS;
-      return NULL;
+#define DEFINE_WASM_API(NAME, ARG, ARG_CHECK) \
+    int32_t NAME(ARG); \
+    static wasm_trap_t* wasmtime_##NAME( \
+        void *env, \
+        wasmtime_caller_t *caller, \
+        const wasmtime_val_t *args, \
+        size_t nargs, \
+        wasmtime_val_t *results, \
+        size_t nresults \
+    ) { \
+        ARG_CHECK \
+        results[0].kind = WASMTIME_I32; \
+        results[0].of.i32 = res; \
+        return NULL; \
     }
 
-    switch (log_level) {
-      case PROXY_LOG_CRITICAL:
-        host_log_level = NGX_LOG_EMERG;
-        break;
+#define DEFINE_WASM_API_ARG_I32 \
+    int32_t
 
-      case PROXY_LOG_ERROR:
-        host_log_level = NGX_LOG_ERR;
-        break;
+#define DEFINE_WASM_API_ARG_CHECK_I32(NAME) \
+    int32_t p1 = args[0].of.i32; \
+    int32_t res = NAME(p1);
 
-      case PROXY_LOG_WARN:
-        host_log_level = NGX_LOG_WARN;
-        break;
+#define DEFINE_WASM_API_ARG_I32_I32_I32 \
+    int32_t, int32_t, int32_t
 
-      case PROXY_LOG_INFO:
-        host_log_level = NGX_LOG_INFO;
-        break;
+#define DEFINE_WASM_API_ARG_CHECK_I32_I32_I32(NAME) \
+    int32_t p1 = args[0].of.i32; \
+    int32_t p2 = args[1].of.i32; \
+    int32_t p3 = args[2].of.i32; \
+    int32_t res = NAME(p1, p2, p3);
 
-      case PROXY_LOG_DEBUG:
-        host_log_level = NGX_LOG_DEBUG;
-        break;
-
-      case PROXY_LOG_TRACE:
-        host_log_level = NGX_LOG_DEBUG;
-        break;
-    }
-
-    ngx_log_error(host_log_level, ngx_cycle->log, 0, "%*s", size, p);
-
-    results[0].kind = WASMTIME_I32;
-    results[0].of.i32 = PROXY_RESULT_OK;
-    return NULL;
-}
+#define DEFINE_WASM_NAME(NAME, ARG) \
+    {ngx_string(#NAME), wasmtime_##NAME, ARG},
+#define DEFINE_WASM_NAME_ARG_I32 \
+    1, {WASM_I32}
+#define DEFINE_WASM_NAME_ARG_I32_I32_I32 \
+    3, {WASM_I32, WASM_I32, WASM_I32}
 
 
 typedef struct {
@@ -85,15 +56,17 @@ typedef struct {
 } ngx_wasm_host_api_t;
 
 
+DEFINE_WASM_API(proxy_set_effective_context,
+                DEFINE_WASM_API_ARG_I32,
+                DEFINE_WASM_API_ARG_CHECK_I32(proxy_set_effective_context))
+DEFINE_WASM_API(proxy_log,
+                DEFINE_WASM_API_ARG_I32_I32_I32,
+                DEFINE_WASM_API_ARG_CHECK_I32_I32_I32(proxy_log))
+
+
 static ngx_wasm_host_api_t host_apis[] = {
-    { ngx_string("proxy_set_effective_context"),
-      proxy_set_effective_context,
-      1, {WASM_I32}
-    },
-    { ngx_string("proxy_log"),
-      proxy_log,
-      3, {WASM_I32, WASM_I32, WASM_I32}
-    },
+    DEFINE_WASM_NAME(proxy_set_effective_context, DEFINE_WASM_NAME_ARG_I32)
+    DEFINE_WASM_NAME(proxy_log, DEFINE_WASM_NAME_ARG_I32_I32_I32)
     { ngx_null_string, NULL, 0, {} }
 };
 
