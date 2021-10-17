57be03dd0c94201c91a27894b4aa02525fd9be81
spacewander <spacewanderlzx@gmail.com>
2021-10-17 20:23:59 +0800

    feat: build wasmtime from source with musl

--- install-wasmtime.sh
+++ install-wasmtime.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+set -euo pipefail -x
 
-wget https://github.com/bytecodealliance/wasmtime/releases/download/v0.30.0/wasmtime-v0.30.0-x86_64-linux-c-api.tar.xz
-tar -xvf ./wasmtime-v0.30.0-x86_64-linux-c-api.tar.xz
-mv wasmtime-v0.30.0-x86_64-linux-c-api wasmtime-c-api
+VER=v0.30.0
+wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-x86_64-linux-c-api.tar.xz
+tar -xvf ./wasmtime-${VER}-x86_64-linux-c-api.tar.xz > /dev/null
+if echo "int main(void) {}" | gcc -o /dev/null -v -x c - &> /dev/stdout| grep collect | tr -s " " "\012" | grep musl; then
+    # build from source code if the libc is musl
+    git clone git@github.com:bytecodealliance/wasmtime -b ${VER} \
+        && cd wasmtime \
+        && git submodule update --init \
+        && cargo build --release --manifest-path crates/c-api/Cargo.toml
+fi
+mv wasmtime-${VER}-x86_64-linux-c-api wasmtime-c-api
