f1e9bf5a70e8e51230060ccf5fc48f2adcdaa1ad
spacewander <spacewanderlzx@gmail.com>
2021-10-19 12:44:51 +0800

    ci: improve clone link

--- install-wasmtime.sh
+++ install-wasmtime.sh
@@ -4,12 +4,14 @@ set -euo pipefail -x
 VER=v0.30.0
 wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-x86_64-linux-c-api.tar.xz
 tar -xvf ./wasmtime-${VER}-x86_64-linux-c-api.tar.xz > /dev/null
+mv wasmtime-${VER}-x86_64-linux-c-api wasmtime-c-api
 if echo "int main(void) {}" | gcc -o /dev/null -v -x c - &> /dev/stdout| grep collect | tr -s " " "\012" | grep musl; then
     # build from source code if the libc is musl
-    git clone git@github.com:bytecodealliance/wasmtime -b ${VER} \
+    git clone https://github.com/bytecodealliance/wasmtime -b ${VER} --depth 1 \
         && cd wasmtime \
         && git submodule update --init \
         && RUSTFLAGS="-C target-feature=-crt-static" \
-           cargo build --release --manifest-path crates/c-api/Cargo.toml
+           cargo build --release --manifest-path crates/c-api/Cargo.toml \
+        && mv target/release/libwasmtime.* ../wasmtime-c-api/lib \
+        && cd ..
 fi
-mv wasmtime-${VER}-x86_64-linux-c-api wasmtime-c-api
