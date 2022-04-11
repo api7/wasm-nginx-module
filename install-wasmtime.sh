566db156ad901c0acb8483da8caaba0830e149e6
soulbird <zhaothreee@gmail.com>
2022-04-11 18:49:19 +0800

    fix: build on arm64

--- install-wasmtime.sh
+++ install-wasmtime.sh
@@ -14,14 +14,16 @@
 # limitations under the License.
 #
 set -euo pipefail -x
+arch=$(uname -m | tr '[:upper:]' '[:lower:]')
 
+ARCH=$arch
 VER=v0.35.1
-wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-x86_64-linux-c-api.tar.xz
-tar -xvf ./wasmtime-${VER}-x86_64-linux-c-api.tar.xz > /dev/null
+wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-${ARCH}-linux-c-api.tar.xz
+tar -xvf ./wasmtime-${VER}-${ARCH}-linux-c-api.tar.xz > /dev/null
 if [ -d wasmtime-c-api ]; then
     rm -rf wasmtime-c-api
 fi
-mv wasmtime-${VER}-x86_64-linux-c-api wasmtime-c-api
+mv wasmtime-${VER}-${ARCH}-linux-c-api wasmtime-c-api
 if echo "int main(void) {}" | gcc -o /dev/null -v -x c - &> /dev/stdout| grep collect | tr -s " " "\012" | grep musl; then
     # build from source code if the libc is musl
     git clone https://github.com/bytecodealliance/wasmtime -b ${VER} --depth 1 \
